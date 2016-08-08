7cbe13f13e13e87797d90f263dafadb1eda0ab7c
LYK <dalinaum@gmail.com>
2016-08-09 06:17:34 +0900

    04-04 추가, 04-05 원형 추가.

---
+++ 04-05-collapsingtoolbar/app/src/main/java/com/example/collapsingtoolbar/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.collapsingtoolbar;
+
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("Collapsing");
+        collapsingToolbarLayout.setExpandedTitleColor(0xffffffff);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.menu_main);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.android) {
+                    Toast.makeText(MainActivity.this, "Hello Android!", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
