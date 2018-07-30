288ea3899b782fecaa165435edb7b2dff6578eaa
LYK <dalinaum@gmail.com>
2018-07-31 01:55:58 +0900

    2.3장의 코드를 변경.

---
+++ 02-03-editbox/java/app/src/main/java/com/example/edittext/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.edittext;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text);
+        editText = findViewById(R.id.editText);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> textView.setText(editText.getText().toString()));
+    }
+}
