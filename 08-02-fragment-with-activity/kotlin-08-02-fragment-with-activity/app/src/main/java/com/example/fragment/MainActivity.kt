1d8bbb9488313a0846585eefd8142262f1005469
LYK <dalinaum@gmail.com>
2019-07-16 20:47:14 +0900

    8.2장 코틀린 자바 코드 작성.

---
+++ 08-02-fragment-with-activity/kotlin-08-02-fragment-with-activity/app/src/main/java/com/example/fragment/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.fragment
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity(), FirstFragment.OnButtonClickedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+
+    override fun onButtonClicked() {
+        Toast.makeText(this, "Hello Fragment!", Toast.LENGTH_LONG).show()
+    }
+}
