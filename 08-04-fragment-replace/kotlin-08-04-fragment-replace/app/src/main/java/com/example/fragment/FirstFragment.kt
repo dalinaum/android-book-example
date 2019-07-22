ad9eb59c7cb5db3e032a54419f35fd985731a385
LYK <dalinaum@gmail.com>
2019-07-22 21:22:30 +0900

    8.4 장 재정리.

---
+++ 08-04-fragment-replace/kotlin-08-04-fragment-replace/app/src/main/java/com/example/fragment/FirstFragment.kt
@@ -0,0 +1,38 @@
+package com.example.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class FirstFragment : Fragment() {
+
+    private var mOnButtonClickedListener: OnButtonClickedListener? = null
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is OnButtonClickedListener) {
+            mOnButtonClickedListener = context
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+        val helloButton = view.findViewById<Button>(R.id.hello_button)
+        helloButton.setOnClickListener { v -> mOnButtonClickedListener!!.onButtonClicked() }
+        return view
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        mOnButtonClickedListener = null
+    }
+
+    interface OnButtonClickedListener {
+        fun onButtonClicked()
+    }
+}
---
+++ 08-04-fragment-replace/kotlin-08-04-fragment-replace/app/src/main/java/com/example/fragment/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity(), FirstFragment.OnButtonClickedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            val firstFragment = FirstFragment()
+            fragmentTransaction.add(R.id.container, firstFragment)
+            fragmentTransaction.commit()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+
+    override fun onButtonClicked() {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val secondFragment = SecondFragment()
+        fragmentTransaction.replace(R.id.container, secondFragment)
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+}
