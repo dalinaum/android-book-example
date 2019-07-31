b217de92bab1531db19b6c4742889e41729a4cb6
LYK <dalinaum@gmail.com>
2019-07-31 22:05:48 +0900

    java 11.1 장 구현.

--- 11-01-broadcast-receiver/java-11-01-broadcast-receiver/app/src/main/java/com/example/broadcast/MainActivity.java
+++ 11-01-broadcast-receiver/java-11-01-broadcast-receiver/app/src/main/java/com/example/broadcast/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,12 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent("com.example.broadcast.FIRST_MESSAGE");
-                sendBroadcast(intent);
-            }
+        findViewById(R.id.button).setOnClickListener(v -> {
+            Intent intent = new Intent("com.example.broadcast.FIRST_MESSAGE");
+            sendBroadcast(intent);
         });
     }
 
