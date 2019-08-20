ae555a2f5aeb7b5d0b3c925be1e9487df291535f
LYK <dalinaum@gmail.com>
2019-08-20 20:44:48 +0900

    12.1장 자바 버전

---
+++ 12-01-shared-element/java-12-01-shared-element/app/src/main/java/com/example/sharedElement/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.sharedElement;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ImageView imageview = findViewById(R.id.imageView);
+        imageview.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, imageview, "transitionImage");
+                startActivity(intent, activityOptions.toBundle());
+            } else {
+                startActivity(intent);
+            }
+        });
+    }
+}
