a1bc0dbc6959795b24872988b4e59ea110b0bec2
LYK <dalinaum@gmail.com>
2019-08-20 22:25:04 +0900

    13.1 서포트 라이브러리 -> 안드로이드 X

--- 13-01-floating-action-button/java-13-01-floating-action-button/app/src/main/java/com/example/floatingactionbutton/MainActivity.java
+++ 13-01-floating-action-button/java-13-01-floating-action-button/app/src/main/java/com/example/floatingactionbutton/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.floatingactionbutton;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
