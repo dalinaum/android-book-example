daf5f59c3e27262982364dbea3bfc9d34da73a9f
LYK <dalinaum@gmail.com>
2019-08-27 20:24:47 +0900

    13.1 코틀린 코드 작성.

---
+++ 13-01-floating-action-button/kotlin-13-01-floating-action-button/app/src/main/java/com/example/floatingactionbutton/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.floatingactionbutton
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<FloatingActionButton>(R.id.button)
+        button.setOnClickListener { view -> Toast.makeText(this@MainActivity, "button clicked!", Toast.LENGTH_SHORT).show() }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+}
