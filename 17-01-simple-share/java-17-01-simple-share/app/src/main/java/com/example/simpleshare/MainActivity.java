b9f54b1ac5b8bc718137bf0c81f58a615de3bbf4
LYK <dalinaum@gmail.com>
2019-10-04 21:54:48 +0900

    17.1장 자바 버전 작성.

--- 17-01-simple-share/java-17-01-simple-share/app/src/main/java/com/example/simpleshare/MainActivity.java
+++ 17-01-simple-share/java-17-01-simple-share/app/src/main/java/com/example/simpleshare/MainActivity.java
@@ -3,22 +3,19 @@ package com.example.simpleshare;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.support.annotation.NonNull;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
+import androidx.core.content.FileProvider;
+import androidx.core.view.MenuItemCompat;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -27,14 +24,20 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView image;
     private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        image = (ImageView) findViewById(R.id.image);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem item = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
             if (checkSelfPermission("android.permission.WRITE_EXTERNAL_STORAGE") == PackageManager.PERMISSION_GRANTED) {
                 new ImageAsyncTask().execute();
@@ -48,14 +51,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             new ImageAsyncTask().execute();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        MenuItem item = menu.findItem(R.id.action_share);
-        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
         return true;
     }
 
@@ -75,7 +70,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void copyImageFromAsset() {
-        String imagePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + File.separator + "cat.jpg";
+
+        try {
+            Log.d("TEST", getFilesDir().getCanonicalPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String imagePath = getFilesDir() + File.separator + "cat.jpg";
+        getCacheDir();
 
         if (new File(imagePath).exists()) {
             return;
@@ -91,41 +94,28 @@ public class MainActivity extends AppCompatActivity {
             in.close();
             out.flush();
             out.close();
-
-            MediaScannerConnection.scanFile(this, new String[]{imagePath}, new String[]{"image/jpeg"}, null);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    class ImageAsyncTask extends AsyncTask<Void, Void, Bitmap> {
+    class ImageAsyncTask extends AsyncTask<Void, Void, Void> {
 
         @Override
-        protected Bitmap doInBackground(Void... voids) {
+        protected Void doInBackground(Void... voids) {
             copyImageFromAsset();
-            InputStream input = null;
-            try {
-                input = getAssets().open("cat.jpg");
-                if (input != null) {
-                    Bitmap bitmap = BitmapFactory.decodeStream(input);
-                    input.close();
-                    return bitmap;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
             return null;
         }
 
         @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            image.setImageBitmap(bitmap);
+        protected void onPostExecute(Void aVoid) {
             Log.d("AAAA", "shareActionProvider" + shareActionProvider);
             if (shareActionProvider != null) {
                 Intent shareIntent = new Intent(Intent.ACTION_SEND);
                 shareIntent.setType("image/jpg");
-                String imagePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + File.separator + "cat.jpg";
-                Uri assetUri = Uri.fromFile(new File(imagePath));
+                String imagePath = getFilesDir() + File.separator + "cat.jpg";
+                File imageFile = new File(imagePath);
+                Uri assetUri = FileProvider.getUriForFile(MainActivity.this, "com.example.simpleshare.fileprovider", imageFile);
                 shareIntent.putExtra(Intent.EXTRA_STREAM, assetUri);
                 shareActionProvider.setShareIntent(shareIntent);
                 Log.d("AAAA", "shareActionProvider set!");
