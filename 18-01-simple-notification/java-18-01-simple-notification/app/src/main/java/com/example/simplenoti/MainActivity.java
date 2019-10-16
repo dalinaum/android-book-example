dda8393b6bc277ed3a37c508dc0f2dc9fc5b98e6
LYK <dalinaum@gmail.com>
2019-10-16 22:47:05 +0900

    18.1 자바 코드 업데이트

---
+++ 18-01-simple-notification/java-18-01-simple-notification/app/src/main/java/com/example/simplenoti/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.simplenoti;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        String channelId = "defaultChannel";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(channelId, "defaultChannel", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            NotificationCompat.Builder builder =
+                    new NotificationCompat.Builder(MainActivity.this, channelId)
+                            .setSmallIcon(R.drawable.ic_noti)
+                            .setContentText("노티피케이션입니다. 안녕하세요.")
+                            .setContentTitle("간단한 노티");
+            int notyId = 1;
+            notificationManager.notify(notyId, builder.build());
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
