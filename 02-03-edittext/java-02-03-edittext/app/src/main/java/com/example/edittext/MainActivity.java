ae7ac2a11db8fd01493805b56ec588a172c41969
LYK <dalinaum@gmail.com>
2019-10-29 23:32:32 +0900

    2장 AndroidX, build.gradle 업데이트

--- 02-03-edittext/java-02-03-edittext/app/src/main/java/com/example/edittext/MainActivity.java
+++ 02-03-edittext/java-02-03-edittext/app/src/main/java/com/example/edittext/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.edittext;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
