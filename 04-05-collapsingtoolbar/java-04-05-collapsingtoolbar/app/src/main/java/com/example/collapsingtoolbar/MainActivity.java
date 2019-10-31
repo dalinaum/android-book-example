e5872cb3344ee548baf3c26faceeec7fa8497791
LYK <dalinaum@gmail.com>
2019-10-31 23:31:25 +0900

    4.5장 현대화

--- 04-05-collapsingtoolbar/java-04-05-collapsingtoolbar/app/src/main/java/com/example/collapsingtoolbar/MainActivity.java
+++ 04-05-collapsingtoolbar/java-04-05-collapsingtoolbar/app/src/main/java/com/example/collapsingtoolbar/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.collapsingtoolbar;
 
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,19 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
         collapsingToolbarLayout.setTitle("IO16");
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.inflateMenu(R.menu.menu_main);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                if (item.getItemId() == R.id.android) {
-                    Toast.makeText(MainActivity.this, "Hello Android!", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                return false;
+        toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.android) {
+                Toast.makeText(MainActivity.this, "Hello Android!", Toast.LENGTH_SHORT).show();
+                return true;
             }
+            return false;
         });
     }
 }
