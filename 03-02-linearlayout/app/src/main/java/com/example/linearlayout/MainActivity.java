7e2b582c421cf9fa00af9f17902c52da6521d468
LYK <dalinaum@gmail.com>
2019-10-31 21:42:15 +0900

    3.2장까지 현대화.

--- 03-02-linearlayout/app/src/main/java/com/example/linearlayout/MainActivity.java
+++ 03-02-linearlayout/app/src/main/java/com/example/linearlayout/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.linearlayout;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
