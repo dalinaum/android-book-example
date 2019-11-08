b980921891907ecba66bf55a84a32aa839281b30
LYK <dalinaum@gmail.com>
2019-11-08 21:46:17 +0900

    6.5장 작업

--- 06-05-listview/java-06-05-listview/app/src/main/java/com/example/listview/MainActivity.java
+++ 06-05-listview/java-06-05-listview/app/src/main/java/com/example/listview/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.listview;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
