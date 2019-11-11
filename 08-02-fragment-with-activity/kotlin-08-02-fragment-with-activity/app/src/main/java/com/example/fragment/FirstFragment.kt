1ce70dd28c37cc412f467832f784d50615b14d97
LYK <dalinaum@gmail.com>
2019-11-12 01:12:29 +0900

    8.2장 업데이트

--- 08-02-fragment-with-activity/kotlin-08-02-fragment-with-activity/app/src/main/java/com/example/fragment/FirstFragment.kt
+++ 08-02-fragment-with-activity/kotlin-08-02-fragment-with-activity/app/src/main/java/com/example/fragment/FirstFragment.kt
@@ -2,7 +2,7 @@ package com.example.fragment
 
 import android.content.Context
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +12,7 @@ class FirstFragment : Fragment() {
 
     private var mOnButtonClickedListener: OnButtonClickedListener? = null
 
-    override fun onAttach(context: Context?) {
+    override fun onAttach(context: Context) {
         super.onAttach(context)
         if (context is OnButtonClickedListener) {
             mOnButtonClickedListener = context
@@ -23,7 +23,7 @@ class FirstFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_first, container, false)
         val helloButton = view.findViewById<Button>(R.id.hello_button)
-        helloButton.setOnClickListener { v -> mOnButtonClickedListener!!.onButtonClicked() }
+        helloButton.setOnClickListener { v -> mOnButtonClickedListener?.onButtonClicked() }
         return view
     }
 
