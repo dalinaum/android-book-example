4d9c45b2f0e4c4b94d649dab51715ced3be6e283
LYK <dalinaum@gmail.com>
2019-11-14 23:28:08 +0900

    12.1장 작업

--- 12-01-shared-element/kotlin-12-01-shared-element/app/src/main/java/com/example/sharedElement/DetailActivity.kt
+++ 12-01-shared-element/kotlin-12-01-shared-element/app/src/main/java/com/example/sharedElement/DetailActivity.kt
@@ -1,7 +1,7 @@
 package com.example.sharedElement
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class DetailActivity : AppCompatActivity() {
 
